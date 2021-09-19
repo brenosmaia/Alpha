2feff63ca4411ec3a29c3f774331166f94f65a62
marcosvmgil <viniciusmarcos_v@hotmail.com>
2021-09-18 23:09:39 -0300

    Updating homeAdm page

--- src/pages/HomeAdm.js
+++ src/pages/HomeAdm.js
@@ -3,9 +3,14 @@ import { Form, Button, Card, Alert, ListGroup } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { useHistory } from "react-router-dom";
+import Board from "react-trello";
+import Modal from "react-modal";
+import CloseButton from "react-bootstrap/CloseButton";
 
 export default function HomeAdm() {
   const [userData, setUserData] = useState({});
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
   const history = useHistory();
 
   const navigateToHome = (event) =>{
@@ -17,6 +22,30 @@ export default function HomeAdm() {
   const navigateToProducts = (event) =>{
     history.push("/produtos");
   }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#343a40",
+      color: "white",
+      display: "flex",
+      width: "700px",
+      flexDirection: "column",
+    },
+  };
 
   return (
     <>
@@ -35,25 +64,162 @@ export default function HomeAdm() {
           </Nav>
         </div>
       </Navbar>
-      <div
-        style={{
-          width: "1910px",
-          height: "950px",
-          marginLeft: "-400px",
-        }}
-      >
-        <Card
-          className="align-item-center"
+      <div className="container">
+        <div
+          className="container"
           style={{
+            borderRadius: "5px",
+            padding: "20px",
             position: "absolute",
-            marginTop: "100px",
-            backgroundColor: "rgb(253 253 253 / 69%)",
-            marginLeft: "850px",
+            marginTop: "40px",
+            width: "100%",
+            backgroundColor: "#ccc",
+            height: "900px",
+            marginLeft: "-20px",
           }}
         >
-          <Card.Body className="text-left">Bem vindo ao Alpha!</Card.Body>
-        </Card>
+          <h2>Criar Pedido</h2>
+          <Form>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "920px",
+              }}
+            >
+              <div>Produtos</div>
+              <div>Quantidade</div>
+            </div>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 1" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 2" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 3" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 4" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 5" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 6" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicCheckbox"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                maxWidth: "900px",
+              }}
+            >
+              <Form.Check type="checkbox" label="item 7" />
+              <Form.Control size="sm" type="text" style={{ width: "50px" }} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="comments.ControlTextarea1">
+              <Form.Label>Observações</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" style={{ width: "100px" }} >
+              Criar
+            </Button>
+          </Form>
+        </div>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <h2>Pedido X</h2>
+          <CloseButton variant="white" onClick={closeModal}></CloseButton>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <h2>Pedido X criado com sucesso</h2>
+        </div>
+      </Modal>
     </>
   );
 }
